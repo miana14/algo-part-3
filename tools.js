bf1c3f8a5e194320c50756ac5e883c3aa0c69215
ThierryBouedo <thierry.bouedo@free.fr>
2023-11-09 14:15:57 +0100

    tools.js updated

--- tools.js
+++ tools.js
@@ -1,16 +1,16 @@
-function displayArray () {
-    let obj = analyze();
+/* 
+ * Display inside an alert box the formatted content of an array
+ * with something like : "index : i -> value" for each item of the array.
+ * 
+ * parameters: obj: array to display
+ * return:  
+ */
+
+function displayArray (obj) {
     let out = 'Contenu du tableau :\n\n';
     for (let i in obj) {
         out += "index : " + i + " -> valeur : " + obj[i] + "\n";
     }
 
     alert(out);
-}
-
-function addStudentToSelect (name, firstname) {
-    let newOption = new Option(firstname + " " + name, name+firstname);
-    const select = document.getElementById ('student-select');
-    select.add(newOption, undefined);
-    return;
 }
